ac1ef68bbd1c9f9e9a9203ed402720b8067d78d2
Anshul Sharma <anshulsharma4445@gmail.com>
2025-05-17 08:37:38 +0530

    fixes-related-to-styling-and-added-custom-errors-for-userLoggedIn-middleware

--- middleware/userLoggedIn.js
+++ middleware/userLoggedIn.js
@@ -1,16 +1,17 @@
 import jwt from 'jsonwebtoken'
+import { createCustomError } from '../errors/customError.js'
 
 const isUserLoggedIn = (req,res,next) => {
 
     if(!req.cookies.token) {
-        res.status(400).json({msg: 'Token is missing'})
+       return next(createCustomError(1503,'Token is missing. Please login to continue'))
     }
 
     const JWT_SECRET_TOKEN = process.env.JWT_SECRET_TOKEN 
     const tokenData = jwt.verify(req.cookies.token, JWT_SECRET_TOKEN)
 
     if(!tokenData) {
-        return res.status(400).json({msg: 'Invalid Token'})
+       return next(createCustomError(1503,'Invalid Token. Please login to continue'))
     }
 
     req.user = tokenData
